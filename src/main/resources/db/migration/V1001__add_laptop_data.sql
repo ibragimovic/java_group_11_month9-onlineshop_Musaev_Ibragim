dfcf3575e36b0c4bc3d99a7145ebafce30b9e6df
MusaevIbragim <m.ibragim88@gmail.com>
2022-05-06 12:40:37 +0600

    filled tables with test data

---
+++ src/main/resources/db/migration/V1001__add_laptop_data.sql
@@ -0,0 +1,3 @@
+ insert into laptops(name, image, description, price) values('MacbookPRO14', 'MacbookPRO14.jpg', 'The most powerful MacBook Pro ever is here.', 2000);
+ insert into laptops(name, image, description, price) values('MacbookAIR13', 'MacbookAIR13.jpg', '13.3-inch (diagonal) LED-backlit display with IPS technology.', 1250);
+ insert into laptops(name, image, description, price) values('ACER ASPIRE', 'AcerAspire.jpg', 'RYZEN R5-4500U/RADEON GRAPHICS (8+256GB SSD)', 645);
---
+++ src/main/resources/db/migration/V1003__add_phones_data.sql
@@ -0,0 +1,3 @@
+insert into phones(name, image, description, price) values('IPHONE 13 PRO MAX', 'iphone_13_ProMAX.jpg', 'The most powerful smartphone.', 1350);
+insert into phones(name, image, description, price) values('Xiaomi POCO', 'XiaomiPOCO.jpg', 'M4 PRO (8+256) EU.', 215);
+insert into phones(name, image, description, price) values('NOKIA 150 DS', 'NOKIA150.jpg', 'The Nokia 150 has everything you need.', 45);
