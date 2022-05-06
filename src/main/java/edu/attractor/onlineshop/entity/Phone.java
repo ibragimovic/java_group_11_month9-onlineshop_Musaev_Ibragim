346f132791cf92ef523eacde11e2503369b4e3ba
MusaevIbragim <m.ibragim88@gmail.com>
2022-05-06 13:01:21 +0600

    created entities

---
+++ src/main/java/edu/attractor/onlineshop/entity/Phone.java
@@ -0,0 +1,33 @@
+package edu.attractor.onlineshop.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "phones")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Phone {
+    @Id
+    @Column
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private Integer price;
+}
