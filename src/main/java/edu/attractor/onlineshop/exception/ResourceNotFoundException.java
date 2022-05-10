3b8104e91f4c45f6080e69d82f2f15f38153c27b
MusaevIbragim <m.ibragim88@gmail.com>
2022-05-10 13:23:23 +0600

    #66 created ResourceNotFoundException class

---
+++ src/main/java/edu/attractor/onlineshop/exception/ResourceNotFoundException.java
@@ -0,0 +1,20 @@
+package edu.attractor.onlineshop.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@Getter @Setter
+public class ResourceNotFoundException extends RuntimeException{
+
+    private String resource;
+    private Long id;
+
+    public ResourceNotFoundException(String resource, Long id) {
+        super();
+        this.resource = resource;
+        this.id = id;
+    }
+}
