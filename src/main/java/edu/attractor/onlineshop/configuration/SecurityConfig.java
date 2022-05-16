7a70b6e1aebfcbc1ebdbe1c24e949dc81bcb8de1
MusaevIbragim <m.ibragim88@gmail.com>
2022-05-16 12:56:21 +0600

    #68 changed the SecurityConfig class

--- src/main/java/edu/attractor/onlineshop/configuration/SecurityConfig.java
+++ src/main/java/edu/attractor/onlineshop/configuration/SecurityConfig.java
@@ -1,17 +1,19 @@
 package edu.attractor.onlineshop.configuration;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    private final DataSource dataSource;
+
     @Bean
     public PasswordEncoder Encoder() {
         return new BCryptPasswordEncoder();
@@ -19,21 +21,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error=true");
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true);
+
         http.authorizeRequests()
-                .antMatchers("/users/**")
-                .fullyAuthenticated();
+                .antMatchers("/profile/**")
+                .authenticated();
+
 
         http.authorizeRequests()
                 .anyRequest()
                 .permitAll();
-
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        http.httpBasic();
-
-        http.formLogin().disable().logout().disable();
-
-        http.csrf().disable();
     }
 }
