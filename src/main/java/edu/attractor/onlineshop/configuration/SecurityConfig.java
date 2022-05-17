fa4745abf019451fdf24603e8905e13a0bfe2265
MusaevIbragim <m.ibragim88@gmail.com>
2022-05-17 15:05:51 +0600

    changed SecurityConfig and application.properties

--- src/main/java/edu/attractor/onlineshop/configuration/SecurityConfig.java
+++ src/main/java/edu/attractor/onlineshop/configuration/SecurityConfig.java
@@ -3,16 +3,21 @@ package edu.attractor.onlineshop.configuration;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
+@EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    private final DataSource dataSource;
+    private final DataSource dataSource;
 
     @Bean
     public PasswordEncoder Encoder() {
@@ -24,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/profile")
                 .failureUrl("/login?error=true");
 
         http.logout()
@@ -40,5 +46,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .anyRequest()
                 .permitAll();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String fetchUsersQuery = "select email, password, enabled"
+                + " from users"
+                + " where email = ?";
+
+        String fetchRolesQuery = "select email, role"
+                + " from users"
+                + " where email = ?";
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(fetchUsersQuery)
+                .authoritiesByUsernameQuery(fetchRolesQuery);
     }
 }
