b6bcb0bdda881e663086d0efec7603ae97a8ef95
MusaevIbragim <m.ibragim88@gmail.com>
2022-05-24 16:25:51 +0600

    #69 created CartController

--- src/main/java/edu/attractor/onlineshop/controller/CartController.java
+++ src/main/java/edu/attractor/onlineshop/controller/CartController.java
@@ -1,30 +1,56 @@
 package edu.attractor.onlineshop.controller;
 
+import edu.attractor.onlineshop.service.CartService;
+import edu.attractor.onlineshop.service.CustomerService;
+import edu.attractor.onlineshop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
+import java.util.ArrayList;
 
 @Controller
+@RequiredArgsConstructor
 public class CartController {
 
-    @GetMapping("/cart")
-    public String cart(Model model, @SessionAttribute(name = Constant.CART_ID, required = false)List<String> cart) {
-        if (cart != null) {
-            model.addAttribute("cartItems", cart);
-        }
-        return "cart";
-    }
+    private final CartService cartService;
+    private final OrderService orderService;
 
-    @PostMapping("/cart")
-    @ResponseBody
-    public boolean addToListRest(@RequestParam String value, @SessionAttribute(name = Constant.CART_ID, required = false) List<String> cart) {
-        if (cart != null) {
-            cart.add(value);
+//    TODO show cart's contain
+//    TODO cart's contain view
+//    TODO delete gadget from the cart
+//    TODO add possibility to change some gadget's quantity
+
+    @PostMapping("/cart/add")
+    public String addToCart(@RequestParam String type,
+                            @RequestParam String name,
+                            HttpSession session,
+                            Authentication authentication) {
+        String customerEmail = authentication.getName();
+
+        if (session != null) {
+            var attr = session.getAttribute(Constant.CART_ID);
+
+            if (attr == null) {
+                session.setAttribute(Constant.CART_ID, new ArrayList<String>());
+            }
+            if (!cartService.isCustomerHasCart(customerEmail)) {
+                cartService.createNewCart(customerEmail);
+            }
+
+            try {
+                var orders = (ArrayList<String>)session.getAttribute(Constant.CART_ID);
+                orders.add(name);
+                orderService.saveOrder(customerEmail, type, name);
+
+            } catch (ClassCastException ignored) {
+
+            }
         }
-        return true;
+        return "redirect:/";
     }
 
     @PostMapping("/cart/empty")
